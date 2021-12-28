6755e92714ec8b7ebab79f48c10ef63f3e1eb75d
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2021-12-27 23:33:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+            debugger;
+  }
+  return gifts;
+}
+
+function writeCards(fromNames = [], eventName) {
+    for (counter = 0; counter < fromNames.length; counter++) {
+        console.log(`Thank you, ${fromNames[counter]} for the wonderful ${eventName} gift!`);
+            debugger;
+    }
+    return fromNames, eventName;
+}
+
+function countdown(subtract = []) {
+    while (subtract >= 0) {
+        console.log(subtract--);
+    }
+    return subtract;
+}
+
+wrapGifts(gifts);
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+countdown(subtract = 10);
+
